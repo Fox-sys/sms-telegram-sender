a6ba536b0420b68ab9c115281001de4b4c4858c7
Your Name <you@example.com>
2026-02-24 00:19:54 +0300

    remove user

--- deployment/entrypoints/entrypoint_migrate.sh
+++ deployment/entrypoints/entrypoint_migrate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
+# Always use /data in container so the DB is created in the mounted volume
+export DB_URL="sqlite+aiosqlite:////data/sms.db"
 exec python -m src.run.alembic_runner upgrade head
